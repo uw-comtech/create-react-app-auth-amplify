b162b61f93881579726ec141332dedbbb311a337
uw-comtech <64388359+uw-comtech@users.noreply.github.com>
2020-04-28 08:40:19 -0700

    Add files via upload

--- public/SMSFormHandler.js
+++ public/SMSFormHandler.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
 
     var firstName = $("#firstName").val(),
         lastName = $("#lastName").val(),
+        mrn = $("#mrn").val(),
         source = window.location.pathname,
         optTimestamp = undefined,
         utcSeconds = Date.now() / 1000,
@@ -16,9 +17,11 @@ $(document).ready(function() {
     e.preventDefault();
 
     if (firstName == "") {
-      $('#form-response').html('<div class="mt-3 alert alert-info" role="alert">Please enter your first name.</div>');
+      $('#form-response').html('<div class="mt-3 alert alert-info" role="alert">Please enter patient first name.</div>');
     } else if (lastName == "") {
-      $('#form-response').html('<div class="mt-3 alert alert-info" role="alert">Please enter your last name.</div>');
+      $('#form-response').html('<div class="mt-3 alert alert-info" role="alert">Please enter patient last name.</div>');
+    } else if (mrn == "") {
+      $('#form-response').html('<div class="mt-3 alert alert-info" role="alert">Please enter patient MRN#.</div>');
     } else if (phone.match(/[^0-9]/gi)) {
       $('#form-response').html('<div class="mt-3 alert alert-info" role="alert">Your phone number contains invalid characters. Please check the phone number that you supplied.</div>');
     } else if (phone.length < 10) {
@@ -35,23 +38,18 @@ $(document).ready(function() {
         'destinationNumber': phone,
         'firstName': firstName,
         'lastName': lastName,
+        'mrn': mrn,
         'source': source,
         'optTimestamp': timestamp.toString()
       });
 
-
-      var usr = localStorage.getItem('CognitoIdentityServiceProvider.259a8icmv6a1rn7fgidvvi4to6.LastAuthUser');
-      var idToken = localStorage.getItem('CognitoIdentityServiceProvider.259a8icmv6a1rn7fgidvvi4to6.'+usr+'.idToken');
       $.ajax({
         type: 'POST',
-        url: 'https://dnb9ews7u5.execute-api.us-west-2.amazonaws.com/v1/register',
-        headers: {
-          'Authorization': idToken,
-          'Content-Type':'application/json'
-        },
+        url: 'https://m4fzv9nhgg.execute-api.us-west-2.amazonaws.com/call/',
+        
         data: data,
         success: function(res) {
-          $('#form-response').html('<div class="mt-3 alert alert-success" role="alert"><p>Congratulations! You&apos;ve successfully registered for SMS Alerts from University of Washington.</p><p>We just sent you a message. Follow the instructions in the message to confirm the subscription. We won&apos;t send any additional messages until we receive your confirmation.</p><p>If you decide you don&apos;t want to receive any additional messages from us, just reply to one of our messages with the keyword STOP.</p></div>');
+          $('#form-response').html('<div class="mt-3 alert alert-success" role="alert"><p>Congratulations! You&apos;ve successfully registered for SMS Alerts from ExampleCorp.</p><p>We just sent you a message. Follow the instructions in the message to confirm your subscription. We won&apos;t send any additional messages until we receive your confirmation.</p><p>If you decide you don&apos;t want to receive any additional messages from us, just reply to one of our messages with the keyword STOP.</p></div>');
           $('#submit').prop('hidden', true);
           $('#unsubAll').prop('hidden', true);
           $('#submit').text('Preferences saved!');
