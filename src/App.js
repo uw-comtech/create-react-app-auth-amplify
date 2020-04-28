ed482e4e276166f5d284d140170bae44668b3654
uw-comtech <64388359+uw-comtech@users.noreply.github.com>
2020-04-28 08:41:10 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ class App extends Component {
     return (
     	<ul>
 		  <li>
-		    <a href="/register.html">Register New Contact for SMS</a>
+		    <a href="/register.html">Register New Contact for UW Medicine COVID-19 Connect</a>
 		  </li>
 		  <li>
-		    <a href="https://124cguf2h5.execute-api.us-west-2.amazonaws.com/prod/pages/designer">QnA Content Designer</a>
+		    <a href="">QnA Content Designer</a>
 		  </li>
 		  <li>
-		    <a href="https://search-qnabot-elastic-1inpourwtv4ol-ttrgwhoiwncjz3re2vvn6slyfa.us-west-2.es.amazonaws.com/_plugin/kibana/">Log Analytics Dashboard</a>
+		    <a href="">Log Analytics Dashboard</a>
 		  </li>
 		</ul>
     );
