e9722a2c3dec379d050ac90b4b5c918bf7eeb06c
uw-comtech <64388359+uw-comtech@users.noreply.github.com>
2020-04-30 14:23:11 -0700

    Add files via upload

--- public/SMSFormHandler.js
+++ public/SMSFormHandler.js
@@ -43,13 +43,23 @@ $(document).ready(function() {
         'optTimestamp': timestamp.toString()
       });
 
+      for ( var i = 0, len = localStorage.length; i < len; ++i ) {
+        var storagekey = localStorage.key( i );
+        // console.log(storagekey);
+        if (storagekey.includes("idToken")) {
+          var idTokenValue = localStorage.getItem( storagekey ); 
+        }
+      }
+      
       $.ajax({
         type: 'POST',
-        url: 'https://m4fzv9nhgg.execute-api.us-west-2.amazonaws.com/call/',
-        
+        url: 'https://5tve7pk601.execute-api.us-west-2.amazonaws.com/call/',
+        headers: {
+          "Authorization": idTokenValue
+        },
         data: data,
         success: function(res) {
-          $('#form-response').html('<div class="mt-3 alert alert-success" role="alert"><p>Congratulations! You&apos;ve successfully registered the patient in the UW Medicine COVID-19 Connect program.</p></div>');
+          $('#form-response').html('<div class="mt-3 alert alert-success" role="alert"><p>Congratulations! You&apos;ve successfully registered for SMS Alerts from ExampleCorp.</p><p>We just sent you a message. Follow the instructions in the message to confirm your subscription. We won&apos;t send any additional messages until we receive your confirmation.</p><p>If you decide you don&apos;t want to receive any additional messages from us, just reply to one of our messages with the keyword STOP.</p></div>');
           $('#submit').prop('hidden', true);
           $('#unsubAll').prop('hidden', true);
           $('#submit').text('Preferences saved!');
